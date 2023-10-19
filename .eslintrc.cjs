7fc58066f5e573a0d32a945446f8070c8774f2b9
Matthew Turk <matthewturk@gmail.com>
2023-10-19 14:06:31 -0700

    Clean skeleton

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,11 @@
 module.exports = {
 	root: true,
 	extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:svelte/recommended",
-        "prettier",
-        "plugin:storybook/recommended"
-    ],
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier'
+	],
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	parserOptions: {
