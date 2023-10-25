3cfe9b2a40888d1b45b9a2c3947358749d1e9858
Matthew Turk <matthewturk@gmail.com>
2023-10-25 14:53:15 -0500

    Had too many nested divs.  Fix columns.

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -52,68 +52,65 @@
 	let narrative: number = -1;
 </script>
 
-<div class="w-5/6">
-	<div class="p-4" transition:fade={{ duration: 200 }}>
-		<ListBox>
-			<div class="h-full w-full flex-col grid grid-cols0 md:grid-cols-3 align-top gap-4">
-				{#each narrativeOptions as option, i}
-					<ListBoxItem
-						class="card p-4 flex justify-center objects-top h-40"
-						bind:group={narrative}
-						name="Item {i}"
-						value={i}
+<ListBox class="w-5/6 grid grid-cols-3">
+	{#each narrativeOptions as option, i}
+		<div>
+			<ListBoxItem
+				class="card card-hover p-4 m-4 flex justify-center objects-top h-40"
+				bind:group={narrative}
+				name="Item {i}"
+				value={i}
+			>
+				<header class="flex h-14 justify-center items-center card-header">
+					<img alt={option.text} class="h-12" src={option.icon} />
+				</header>
+				<section class="pt-2 h-10 items-center justify-center text-center">
+					{option.text}
+				</section>
+				<footer class="card-footer p-4 text-center"><h3>{option.title}</h3></footer>
+			</ListBoxItem>
+		</div>
+	{/each}
+</ListBox>
+<hr />
+<div class="w-5/6 flex-1 grid grid-cols-3">
+	<ListBox class="m-4 p-4 col-span-1">
+		{#each motivationOptions as option, i}
+			<ListBoxItem bind:group={need} name={option.value} value={i}
+			class="bg-surface-700"
+				><svelte:fragment slot="lead"
+					><img width="50px" alt={option.title} src={option.icon} /></svelte:fragment
+				>{option.title}</ListBoxItem
+			>
+		{/each}
+	</ListBox>
+	{#if need >= 0}
+		<div class="m-4 p-4 col-span-2 rounded-lg bg-surface-600">
+			{motivationOptions[need]['text']}
+		</div>
+	{/if}
+</div>
+<hr />
+{#if need == 0}
+	<div class="w-5/6 flex-1 grid-grid-cols-3">
+		<div class="p-4" transition:fade={{ duration: 200 }}>
+			<ListBox>
+				<div class="w-full text-token grid grid-cols-1 md:grid-cols-2 gap-4">
+					{#each Array.from({ length: 8 }) as _, i}
+						<ListBoxItem
+							class="card p-4 flex justify-center items-center"
+							bind:group={thing}
+							name="Item {i}"
+							value={i}
 						>
-						<header class="flex h-14 justify-center items-center card-header">
-							<img alt={option.text} class="h-12" src={option.icon} />
-						</header>
-						<section class="pt-2 h-10 items-center justify-center text-center">{option.text}</section>
-						<footer class="card-fooder p-4 text-center"><h3>{option.title}</h3></footer>
-					</ListBoxItem>
-				{/each}
-			</div>
-		</ListBox>
+							Item {i}
+							lorem ipsum<br />
+							just lots of stuff<br />
+							fjweihvjrio
+						</ListBoxItem>
+					{/each}
+				</div>
+			</ListBox>
+		</div>
 	</div>
-</div>
-
-<div class="w-full flex flex-col">
-	<div class="w-full flex flex-row">
-		<ListBox class="m-4 p-4 w-1/3">
-			{#each motivationOptions as option, i}
-				<ListBoxItem bind:group={need} name={option.value} value={i}
-					><svelte:fragment slot="lead"
-						><img width="50px" alt={option.title} src={option.icon} /></svelte:fragment
-					>{option.title}</ListBoxItem
-				>
-			{/each}
-		</ListBox>
-
-		{#if need >= 0}
-			<div class="m-4 p-4 w-1/2 rounded-lg bg-surface-700">
-				{motivationOptions[need]['text']}
-			</div>
-		{/if}
-	</div>
-	<div class="w-5/6">
-		{#if need == 0}
-			<div class="p-4" transition:fade={{ duration: 200 }}>
-				<ListBox>
-					<div class="w-full text-token grid grid-cols-1 md:grid-cols-2 gap-4">
-						{#each Array.from({ length: 8 }) as _, i}
-							<ListBoxItem
-								class="card p-4 flex justify-center items-center"
-								bind:group={thing}
-								name="Item {i}"
-								value={i}
-							>
-								Item {i}
-								lorem ipsum<br />
-								just lots of stuff<br />
-								fjweihvjrio
-							</ListBoxItem>
-						{/each}
-					</div>
-				</ListBox>
-			</div>
-		{/if}
-	</div>
-</div>
+{/if}
