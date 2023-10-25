71586913a0a110505c3f675a54676d4a4063713b
Matthew Turk <matthewturk@gmail.com>
2023-10-25 14:13:49 -0500

    Start porting girder auth to svelte

---
+++ src/lib/GirderAuthentication/Authentication.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+let tabSet: number = 0;
+import {TabGroup, Tab} from "@skeletonlabs/skeleton";
+
+</script>
+
+<TabGroup>
+	<Tab bind:group={tabSet} name="tab1" value={0}>
+		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+		<span>(label 1)</span>
+	</Tab>
+	<Tab bind:group={tabSet} name="tab2" value={1}>(label 2)</Tab>
+	<Tab bind:group={tabSet} name="tab3" value={2}>(label 3)</Tab>
+	<!-- Tab Panels --->
+	<svelte:fragment slot="panel">
+		{#if tabSet === 0}
+			(tab panel 1 contents)
+		{:else if tabSet === 1}
+			(tab panel 2 contents)
+		{:else if tabSet === 2}
+			(tab panel 3 contents)
+		{/if}
+	</svelte:fragment>
+</TabGroup>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,14 +1,5 @@
 <script lang="ts">
+	import Authentication from '$lib/GirderAuthentication/Authentication.svelte';
 </script>
 
-<form method="POST" class="rounded bg-gray-400 m-2 p-4 w-1/2" action="?/login">
-	<label class="label">
-		<span>Username</span>
-		<input class="input" type="text" name="name" placeholder="username" />
-	</label>
-	<label class="label">
-		<span>Password</span>
-		<input class="input" type="password" name="password" />
-	</label>
-	<button type="button" class="btn variant-filled">Login</button>
-</form>
+<Authentication></Authentication>
