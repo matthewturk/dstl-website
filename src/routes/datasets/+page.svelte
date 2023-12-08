62000ec1d368a0497d3ca75c5006e285e4bda62c
Iishi Patel <iishipatel@Iishis-Air.attlocal.net>
2023-12-07 23:23:10 -0800

    chart creation capabilities

--- src/routes/datasets/+page.svelte
+++ src/routes/datasets/+page.svelte
@@ -1,7 +1,12 @@
+
 <script lang="ts">
   import { FileButton } from "@skeletonlabs/skeleton";
   let files: FileList = [];
   let jsonDataArray = []; // Store JSON data
+  let xColumn = ""; // Selected column for X-axis
+  let yColumn = ""; // Selected column for Y-axis
+  let xColumns: string[] = []; // Available X-axis columns
+  let yColumns: string[] = []; // Available Y-axis columns
 
   async function parseCSVToJson(file) {
     return new Promise((resolve, reject) => {
@@ -20,12 +25,21 @@
 
   async function convertFilesToJson() {
     jsonDataArray = []; // Reset previous data
+    xColumns = []; // Reset X-axis columns
+    yColumns = []; // Reset Y-axis columns
+
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
       const jsonData = await parseCSVToJson(file);
       jsonDataArray.push(jsonData);
     }
-    displayJsonData(); // Display JSON data
+
+    if (jsonDataArray.length > 0) {
+      const columns = Object.keys(jsonDataArray[0][0] || {});
+      xColumns = columns;
+      yColumns = columns;
+    }
+    displayJsonData()
   }
 
   function displayJsonData() {
@@ -36,6 +50,49 @@
       jsonDataElement.textContent = `File ${index + 1}: \n${JSON.stringify(data, null, 2)}`;
       jsonDataDisplay.appendChild(jsonDataElement);
     });
+  }
+
+  function visualizeData() {
+    if (xColumn && yColumn) {
+      const canvas = document.getElementById("chartCanvas");
+      const ctx = canvas.getContext("2d");
+
+      const data = {
+        labels: jsonDataArray[0].map((item) => item[xColumn]),
+        datasets: [
+          {
+            label: yColumn,
+            data: jsonDataArray[0].map((item) => item[yColumn]),
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderColor: "rgba(75, 192, 192, 1)",
+            borderWidth: 1,
+          },
+        ],
+      };
+
+      new Chart(ctx, {
+        type: "bar", // Change chart type as needed (e.g., 'line', 'pie', 'radar', etc.)
+        data: data,
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            xAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: xColumn,
+              },
+            }],
+            yAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: yColumn,
+              },
+            }],
+          },
+        },
+      });
+    }
   }
 </script>
 
@@ -51,3 +108,24 @@ You have selected:
 <div id="json-data-display">
   <!-- JSON data will be displayed here -->
 </div>
+<div>
+  {#if jsonDataArray.length > 0}
+    <label>Select X-axis Column: </label>
+    <select bind:value={xColumn}>
+      {#each xColumns as column}
+        <option value={column}>{column}</option>
+      {/each}
+    </select>
+
+    <label>Select Y-axis Column: </label>
+    <select bind:value={yColumn}>
+      {#each yColumns as column}
+        <option value={column}>{column}</option>
+      {/each}
+    </select>
+
+    <button on:click={visualizeData}>Visualize</button>
+  {/if}
+</div>
+
+<canvas id="chartCanvas"></canvas>
