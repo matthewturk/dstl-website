bc21b2099c7b6814bb77daf8cd2ab6fbbf10d497
Matthew Turk <matthewturk@gmail.com>
2024-02-06 13:08:23 -0600

    Updating some of the code

--- src/routes/datasets/+page.svelte
+++ src/routes/datasets/+page.svelte
@@ -1,110 +1,99 @@
 <script lang="ts">
 	import MarkdownContent from '$lib/MarkdownContent.svelte';
+	import * as Papa from 'papaparse';
 	import { FileButton } from '@skeletonlabs/skeleton';
-	import  JSONTree  from 'svelte-json-tree';
-	let files: FileList = [];
-	let jsonDataArray = []; // Store JSON data
-  let value = [];
+	import JSONTree from 'svelte-json-tree';
+	import { Line, Scatter, Bar } from 'svelte-chartjs';
+	//import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, LinearScale, PointElement, CategoryScale } from 'chart.js';
+	import 'chart.js/auto';
+	import type { ChartData, ChartDataset } from 'chart.js';
+	//borderColor: 'rgba(75, 192, 192, 1)',
+
+	let plotType = "line";
+	let files: FileList;
+	let value: IValueArray[] = [];
+	let fields: string[] = [];
+	let dataToPlot: ChartData<any, number[]> = { labels: [], datasets: [] };
 	let xColumn = ''; // Selected column for X-axis
 	let yColumn = ''; // Selected column for Y-axis
-	let xColumns: string[] = []; // Available X-axis columns
-	let yColumns: string[] = []; // Available Y-axis columns
 
-	async function parseCSVToJson(file) {
+	interface IValueArray{
+		[key: string]: number;
+	}
+
+	const parseCSV = (file: File): Promise<Papa.ParseResult<any>> => {
 		return new Promise((resolve, reject) => {
 			Papa.parse(file, {
-				header: true,
+				complete: resolve,
 				dynamicTyping: true,
-				complete: (results) => {
-					resolve(results.data);
-				},
-				error: (error) => {
-					reject(error);
-				}
+				header: true,
+				error: reject
 			});
 		});
+	};
+
+	$: {
+		dataToPlot = {
+			labels: value.map((e) => e[xColumn]),
+			datasets: [{ label: yColumn, data: value.map(e => e[yColumn]) }]
+		};
 	}
 
-	async function convertFilesToJson() {
-		const newDataArray = []; // Reset previous data
-		xColumns = []; // Reset X-axis columns
-		yColumns = []; // Reset Y-axis columns
+	async function parseFile() {
+		console.log(files[0]);
 
-		for (let i = 0; i < files.length; i++) {
-			const file = files[i];
-			const jsonData = await parseCSVToJson(file);
-			newDataArray.push(jsonData);
+		let results: Papa.ParseResult<any>;
+		try {
+			results = await parseCSV(files[0]);
+		} catch (error) {
+			console.error('Error:', error);
+			return;
 		}
 
-		if (jsonDataArray.length > 0) {
-			const columns = Object.keys(newDataArray[0][0] || {});
-			xColumns = columns;
-			yColumns = columns;
-		}
-    value = [...newDataArray];
-	}
-
-	function visualizeData() {
-		if (xColumn && yColumn && jsonDataArray.length > 0) {
-			const canvas = document.getElementById('chartCanvas');
-			canvas.width = 600; // Set desired width
-			canvas.height = 400; // Set desired height
-			const ctx = canvas.getContext('2d');
-
-			const data = {
-				labels: jsonDataArray[0].map((item) => item[xColumn]),
-				datasets: [
-					{
-						label: yColumn,
-						data: jsonDataArray[0].map((item) => item[yColumn]),
-						backgroundColor: 'rgba(75, 192, 192, 0.2)',
-						borderColor: 'rgba(75, 192, 192, 1)',
-						borderWidth: 1
-					}
-				]
-			};
-
-			new Chart(ctx, {
-				type: 'bar',
-				data: data,
-				options: {
-					responsive: false, // Disable responsiveness
-					maintainAspectRatio: false
-				}
-			});
-		}
+		fields = [...(results.meta.fields || [])];
+		value = [...results.data];
 	}
 </script>
 
 <MarkdownContent pagename="datasets">
 	<div class="w-5/6 grid grid-cols-2">
 		<div class="p-4 m-4 flex objects-top">
-			<FileButton name="files" multiple="true" bind:files on:change={convertFilesToJson} />
+			<FileButton name="file" multiple="false" bind:files on:change={parseFile} />
+			<select id="plotType" bind:value={plotType}>
+				<option value="line">Line</option>
+				<option value="scatter">Scatter</option>
+				<option value="bar">Bar</option>
+			</select>
 		</div>
-
 		<div id="json-data-display" class="p-4 m-4 flex objects-top">
 			<JSONTree {value} />
 		</div>
 	</div>
-	<div classs="w-5/6">
-		{#if jsonDataArray.length > 0}
+	<div class="w-5/6 bg-surface-500">
+		{#if value.length > 0}
 			<label for="xcol">Select X-axis Column: </label>
 			<select id="xcol" bind:value={xColumn}>
-				{#each xColumns as column}
-					<option value={column}>{column}</option>
+				{#each fields as field}
+					<option value={field}>{field}</option>
 				{/each}
 			</select>
 
 			<label for="ycol">Select Y-axis Column: </label>
 			<select id="ycol" bind:value={yColumn}>
-				{#each yColumns as column}
-					<option value={column}>{column}</option>
+				{#each fields as field}
+					<option value={field}>{field}</option>
 				{/each}
 			</select>
-
-			<button on:click={visualizeData}>Visualize</button>
 		{/if}
 	</div>
 
-	<canvas id="chartCanvas" />
+	{#if plotType == "line"}
+	<Line class="w-5/6 bg-white h-32" options={{responsive:true}} data={dataToPlot} />
+	{:else if plotType == "scatter"}
+	<Scatter class="w-5/6 bg-white h-32" options={{responsive:true}} data={dataToPlot} />
+	{:else if plotType == "bar"}
+	<Bar class="w-5/6 bg-white h-32" options={{responsive:true}} data={dataToPlot} />
+	{:else}
+		<p>Plot type not supported</p>
+	{/if}
 </MarkdownContent>
