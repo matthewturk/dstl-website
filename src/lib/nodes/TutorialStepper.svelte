ff83783f18a94e8df9cba3a949eb536ccd87c7c7
Matthew Turk <matthewturk@gmail.com>
2024-03-26 09:02:42 -0500

    Close panel on complete

--- src/lib/nodes/TutorialStepper.svelte
+++ src/lib/nodes/TutorialStepper.svelte
@@ -53,7 +53,7 @@
 
 {#if visible}
 	<Panel position="top-left" class="w-1/4">
-		<Stepper class="absolute w-full text-token card p-4 m-4" on:step={updateStep}>
+        <Stepper class="absolute w-full text-token card p-4 m-4" on:step={updateStep} on:complete={closePanel}>
 			<Step>
 				<svelte:fragment slot="header">Welcome!</svelte:fragment>
 				<p>Welcome to the <i>prototype</i> DSTL visualization dashboard.</p>
